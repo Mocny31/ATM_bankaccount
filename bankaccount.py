e97e69eb7f804e656c90f29e297751d329594cf2
Michał Mocny <michalmocny31@gmail.com>
2023-03-10 18:57:12 +0100

    feat: Add typing and optimized docstrings

--- bankaccount.py
+++ bankaccount.py
@@ -16,6 +16,9 @@ Usage:
 - Call the deposit or withdraw method to make a transaction.
 - Call the check_balance_account method to check the account balance.
 - Call the __str__ method to get a string representation of the account.
+
+Author: Michal Mocny
+Date: 05.03.2023
 """
 
 
@@ -24,11 +27,11 @@ class Result:
     Class representing the result of an operation.
 
     Args:
-    message (str): A message describing the result of the operation.
-    value (float): An optional argument containing the value returned by the operation.
+    message: A message describing the result of the operation.
+    value: An optional argument containing the value returned by the operation.
     """
 
-    def __init__(self, message, value=None):
+    def __init__(self, message: str, value: float = None) -> None:
         self.is_success = None
         self.message = message
         self.value = value
@@ -39,11 +42,11 @@ class Ok(Result):
     Class representing the result of a successful operation.
 
     Args:
-    message (str): A message describing the result of the operation.
-    value (float): An optional argument containing the value returned by the operation.
+    message: A message describing the result of the operation.
+    value: An optional argument containing the value returned by the operation.
     """
 
-    def __init__(self, message, value=None):
+    def __init__(self, message: str, value: float = None) -> None:
         super().__init__(message, value)
         self.is_success = True
 
@@ -53,35 +56,36 @@ class Error(Result):
     Class representing the result of a failed operation.
 
     Args:
-    message (str): A message describing the result of the operation.
-    value (float): An optional argument containing the value returned by the operation.
+    message: A message describing the result of the operation.
+    value: An optional argument containing the value returned by the operation.
     """
 
-    def __init__(self, message, value=None):
+    def __init__(self, message: str, value: float = None) -> None:
         super().__init__(message, value)
         self.is_success = False
 
 
 class BankAccount:
     """
-    Class representing a bank account.
+    Class representing a bank account, has methods for depositing,
+    withdrawing and checking the balance.
 
     Args:
-    balance (float): An optional argument specifying the initial account balance.
+    balance: An optional argument specifying the initial account balance.
     """
     next_id = 1
 
-    def __init__(self, balance: float = 0):
+    def __init__(self, balance: float = 0) -> None:
         self.balance = balance
         self.user_id = BankAccount.next_id
         BankAccount.next_id += 1
 
-    def deposit(self, amount):
+    def deposit(self, amount: int) -> Ok:
         """
         Method for making a deposit into a bank account.
 
         Args:
-        amount (float): The amount to be deposited into the account.
+        amount: The amount to be deposited into the account.
 
         Returns:
         Ok: An object representing a successful result of the operation.
@@ -89,12 +93,12 @@ class BankAccount:
         self.balance += amount
         return Ok("Deposit made successfully", amount)
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: int) -> (Ok | Error):
         """
         Method for making a withdrawal from a bank account.
 
         Args:
-        amount (float): The amount to be withdrawn from the account.
+        amount: The amount to be withdrawn from the account.
 
         Returns:
         Ok: An object representing a successful result of the operation.
@@ -103,21 +107,14 @@ class BankAccount:
         if self.balance >= amount:
             self.balance -= amount
             return Ok("Withdraw made successfully.", amount)
-        else:
-            return Error("The account balance is too low, you cannot make a withdrawal.", amount)
 
-    def check_balance_account(self):
+        return Error("The account balance is too low, you cannot make a withdrawal.", amount)
+
+    def check_balance_account(self) -> str:
         """
         Method for checking the balance of a bank account.
-
-        Returns:
-        str: A string representation of the account balance.
         """
         return f"Your account balance: {self.balance}"
 
-    def __str__(self):
-        """
-        Returns:
-        str: A string representation of the bank account.
-        """
+    def __str__(self) -> str:
         return f"The account is assigned id nr {self.user_id} and its balance is {self.balance}"
