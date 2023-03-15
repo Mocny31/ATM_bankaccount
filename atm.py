188f4e651d44258f27ff52a3ed0b751151c81b54
Michał Mocny <michalmocny31@gmail.com>
2023-03-16 00:09:28 +0100

    feat!: change structure of the project
    create file main.py with method main
    add class ATM with methods in atm.py
    add method get_starting_account_balance method in main.py
    refactor the code

--- atm.py
+++ atm.py
@@ -1,77 +1,49 @@
-"""
-ATM system for the STRONG bank.
-
-This script implements a simple ATM system for the STRONG bank
-using the BankAccount class from the bankaccount module.
-
-Usage:
-    - Enter your starting bank account balance when prompted.
-    - Choose an action from the menu by entering the corresponding number.
-    - Follow the prompts to complete the selected action.
-
-Actions:
-    1. Deposit: Add funds to the account.
-    2. Withdraw: Remove funds from the account.
-    3. Check balance: Display the current balance of the account.
-    4. Close: Exit the ATM system.
-
-This script requires the following modules to be imported:
-    - enum
-    - bankaccount
-
-Author: Michal Mocny
-Date: 05.03.2023
-"""
-
-from enum import IntEnum
 from bankaccount import BankAccount
 
 
-def main():
-    while True:
-        try:
-            started_balance = float(
-                input("Enter your starting bank account balance: "))
-            my_account = BankAccount(started_balance)
-        except ValueError:
-            print("Wrong value. Enter a number value.")
+class ATM:
+    """
+    A class that represents an ATM machine.
+
+    Attributes:
+    account: BankAccount
+        An object representing the bank account that will be used for transactions.
+
+
+
+    check_balance() -> str:
+        Returns a string message representing the current balance of the bank account.
+    """
+
+    def __init__(self, started_balance: float = 0):
+        self.account = BankAccount(started_balance)
+
+    def deposit_funds(self, value: int) -> str:
+        """
+        Deposits the specified amount to the bank account.
+        Returns a string message representing the success or failure of the transaction.
+        """
+        if value <= 0:
+            result = "Wrong value. The value must be a positive."
+            return result
         else:
-            if started_balance >= 0:
-                break
+            result = self.account.deposit(value)
+            return result.message
 
-            print("The value must be a positive value or 0.")
+    def withdraw_funds(self, value: int) -> str:
+        """
+        Withdraws the specified amount from the bank account.
+        Returns a string message representing the success or failure of the transaction.
+        """
+        if value <= 0:
+            result = "Wrong value. The value must be a positive."
+            return result
+        else:
+            result = self.account.withdraw(value)
+            return result.message
 
-    menu_atm = IntEnum(
-        "menu_atm", ["Deposit", "Withdraw", "Check_balance", "Close"])
-
-    while True:
-        try:
-            disposition = int(input("""
-            Welcome to STRONG's ATM. Choose what you want to do.
-            1. Deposit
-            2. Withdraw
-            3. Check_balance
-            4. Close
-            Your choose: """))
-            if disposition == menu_atm.Deposit:
-                value = int(input("Enter the amount to be deposited: "))
-                if value <= 0:
-                    print("Wrong value.")
-                else:
-                    result = my_account.deposit(value)
-                    print(result.message)
-            elif disposition == menu_atm.Withdraw:
-                value = int(input("Enter the amount to be withdrawn: "))
-                if value <= 0:
-                    print("Wrong value.")
-                else:
-                    result = my_account.withdraw(value)
-                    print(result.message)
-            elif disposition == menu_atm.Check_balance:
-                print(my_account.check_balance_account())
-            elif disposition == menu_atm.Close:
-                break
-            else:
-                print("Wrong value.")
-        except ValueError:
-            print("Wrong value.")
+    def check_balance(self) -> str:
+        """
+        Returns a string message representing the current balance of the bank account.
+        """
+        return self.account.check_balance_account()
--- main.py
+++ main.py
@@ -1,4 +1,58 @@
-from atm import main
+from enum import IntEnum
+from atm import ATM
+
+
+def get_starting_account_balance():
+    """
+    A method for determining the starting balance of the bank account,
+    created for testing the application.
+    """
+    while True:
+        try:
+            started_account_balance = float(
+                input("Enter your starting bank account balance: "))
+            if started_account_balance >= 0:
+                return started_account_balance
+            print("The value must be a positive value or 0.")
+        except ValueError:
+            print("Wrong value. Enter a float value.")
+
+
+def main():
+    started_account_balance = get_starting_account_balance()
+    session = ATM(started_account_balance)
+
+    menu_atm = IntEnum(
+        "menu_atm", ["Deposit", "Withdraw", "Check_balance", "Close"])
+
+    while True:
+        try:
+            disposition = int(input("""
+            Welcome to STRONG's ATM. Choose what you want to do.
+            1. Deposit
+            2. Withdraw
+            3. Check_balance
+            4. Close
+            Your choose: """))
+            if disposition == menu_atm.Deposit:
+                value = int(input("Enter the amount to be deposited: "))
+                result = session.deposit_funds(value)
+                print(result)
+            elif disposition == menu_atm.Withdraw:
+                value = int(input("Enter the amount to be withdrawn: "))
+                result = session.withdraw_funds(value)
+                print(result)
+            elif disposition == menu_atm.Check_balance:
+                result = session.check_balance()
+                print(result)
+            elif disposition == menu_atm.Close:
+                print("Bye bye.")
+                break
+            else:
+                print("Wrong value.")
+        except ValueError:
+            print("Wrong value.")
+
 
 if __name__ == '__main__':
     main()
